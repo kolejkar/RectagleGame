e452273fe9667a3423d8e649141b4affb39af33a
karpom000 <karpom000@utp.edu.pl>
2025-04-25 16:35:43 +0200

    Add trap object.

---
+++ RectagleGame/Objects/Trap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RectagleGame.Objects
+{
+    public class Trap : Objekt, ICanvas
+    {
+        private bool activeted;
+
+        public Trap()
+        {
+            activeted = true;
+        }
+
+        public void Paint()
+        {
+            System.Console.SetCursorPosition(left, top);
+            if (activeted == true)
+            {
+                System.Console.ForegroundColor = ConsoleColor.DarkRed;
+            }
+            System.Console.Write("T");
+            System.Console.ResetColor();
+        }
+
+        public bool IsUsed()
+        {
+            return !activeted;
+        }
+
+        public void HitHero(Hero hero)
+        {
+            if (left == hero.left && top == hero.top && activeted)
+            {
+                hero.Helth--;
+                activeted = false;
+            }
+        }
+    }
+}
---
+++ RectsgleGameTests/Tests/TrapClassTests.cs
@@ -0,0 +1,70 @@
+﻿using RectagleGame.Objects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace RectsgleGameTests.Tests
+{
+    public class TrapClassTests
+    {
+        [Fact]
+        public void ShouldCreateAndActivateTrap()
+        {
+            //Arrage            
+            //Act
+            Trap trap = new Trap();
+            //Assert
+            Assert.False(trap.IsUsed());
+        }
+
+        [Fact]
+        public void ShouldHeroUsedTrap()
+        {
+            //Arrage
+            Hero hero = new Hero();
+            hero.top = 5;
+            hero.left = 5;
+            Trap trap = new Trap();
+            trap.top = 5;
+            trap.left = 5;
+            //Act
+            trap.HitHero(hero);
+            //Assert
+            Assert.True(trap.IsUsed());
+        }
+
+        [Fact]
+        public void ShouldHeroNotUsedTrap()
+        {
+            //Arrage
+            Hero hero = new Hero();
+            hero.top = 6;
+            hero.left = 6;
+            Trap trap = new Trap();
+            trap.top = 5;
+            trap.left = 5;
+            //Act
+            trap.HitHero(hero);
+            //Assert
+            Assert.False(trap.IsUsed());
+        }
+
+        [Fact]
+        public void ShouldTrapDamagedHeroOnUsedTrap()
+        {
+            //Arrage
+            Hero hero = new Hero();
+            hero.Helth = 10;
+            hero.top = 5;
+            hero.left = 5;
+            Trap trap = new Trap();
+            trap.top = 5;
+            trap.left = 5;
+            //Act
+            trap.HitHero(hero);
+            //Assert
+            Assert.Equal(9, hero.Helth);
+        }
+    }
+}
